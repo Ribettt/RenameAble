c78ca2cfca2aa6eac6e18c14347ba44dabc9d194
VeguiIzumi <64743287+VeguiIzumi@users.noreply.github.com>
2020-08-07 00:54:15 +0700

    Aaa

---
+++ src/main.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const reClient = require('./structures/reClient');
+
+const client = new reClient({
+    prefix: 'ai!',
+    ownerID: ['271576733168173057', '369507341973979136'],
+    disableEveryone: true,
+});
+
+require('./events/ready')(client);
+
+client.login(process.env.TOKEN);
---
+++ src/structures/reClient.js
@@ -0,0 +1,8 @@
+const { AkairoClient } = require('discord-akairo');
+
+module.exports = class reClient extends AkairoClient {
+    constructor(opt) {
+        super(opt);
+
+    }
+};
