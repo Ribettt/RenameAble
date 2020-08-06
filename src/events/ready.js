f214b8165bcc0047a46a193bd73107a4e3920c89
VeguiIzumi <64743287+VeguiIzumi@users.noreply.github.com>
2020-08-07 01:08:08 +0700

    A presence

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,5 +1,10 @@
 module.exports = client => {
     client.on('ready', () => {
+        client.user.setPresence({
+            activity: {
+                name: 'Discord-Aakairo JS',
+            },
+        });
         console.log(client.user.username + ' Ready');
     });
 };
