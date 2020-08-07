add7c620e444be5cff2606f91947ab9fbe676f45
Vegui Izumi <64743287+VeguiIzumi@users.noreply.github.com>
2020-08-07 09:42:28 +0700

    UpdateOwner(s)

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ const reClient = require('./structures/reClient');
 
 const client = new reClient({
     prefix: 'ai!',
-    ownerID: ['271576733168173057', '369507341973979136'],
+    ownerID: ['271576733168173057', '369507341973979136', '695049782942498917'],
     disableMentions: "everyone",
 });
 
