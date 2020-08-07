b0620ad8d0b157fe4fdd49352f1311bc40c5d797
f077 <anos.voldigoad07@gmail.com>
2020-08-07 10:07:14 +0700

    .

--- src/structures/reClient.js
+++ src/structures/reClient.js
@@ -2,7 +2,8 @@ const { AkairoClient } = require('discord-akairo');
 
 module.exports = class reClient extends AkairoClient {
     constructor(opt) {
-        super(opt);
-
+        super({
+            ownerID: ['271576733168173057', '369507341973979136', '695049782942498917']
+        }, opt);
     }
 };
